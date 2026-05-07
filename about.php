7917fd4df09c3ed34c04849f26a3be8054145eaf
andreagonz22 <andrea.gonzalez.raices2026@gmail.com>
2026-05-07 08:51:56 -0600

    linkeo

--- about.php
+++ about.php
@@ -11,6 +11,7 @@ require_once __DIR__ . '/auth_check.php';
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="css/styles.css" />
   <link rel="stylesheet" href="css/about.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <link rel="stylesheet" href="css/navbar_auth.css">
 </head>
 <body>
