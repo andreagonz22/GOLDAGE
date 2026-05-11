f820cb5dcb6ad80cc49351ef3c074b8a7d013566
andreagonz22 <andrea.gonzalez.raices2026@gmail.com>
2026-05-11 11:01:43 -0600

    colores

--- index.php
+++ index.php
@@ -12,9 +12,10 @@ if (session_status() === PHP_SESSION_NONE) {
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
 
-  <link rel="stylesheet" href="css/styles.css" />
+  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <link rel="stylesheet" href="css/navbar_auth.css">
+  <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
